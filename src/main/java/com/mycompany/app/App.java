ad62a254586a40bf20aebc6e7bbcad87b2c4a9f5
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-02-27 05:25:23 +0000

    Student

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,21 +11,18 @@ import java.util.Date;
 import java.lang.Math;
 //import user-built class
 import Book;
+import Student;
 
 
 
 public class App {
     public static void main(String[] args) {
-        int SiSoLop;
-        SiSoLop = 40;
-        Date d = new Date();
-        double ran = Math.random();
-        System.out.println(SiSoLop);
-        System.out.println(d);
-        System.out.println(ran);
+       
 
         Book myObj = new Book("a","b",2);
         System.out.println(myObj.title);
+        Student myStu = new Student("A","B",10);
+        System.out.println(myStu.StudentID);
     }
 }
 
