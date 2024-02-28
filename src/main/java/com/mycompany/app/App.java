39abac196ea4cab855c27d5c1697068e5b43d115
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-02-28 03:22:40 +0000

    Inner class - Use class inside class

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,26 +15,38 @@ import Student;
 import Time;
 import Recursion;
 import List;
+import com.mycompany.app.InnerClass;
 
 
 public class App {
     public static void main(String[] args) {
        
-
+        //Test Book
         Book myObj = new Book("a","b",2);
         System.out.println(myObj.title);
         Student myStu = new Student();
         myStu.StudentID = "abcd";
         System.out.println(myStu.StudentID);
+
+        //Test Time
         Time myTime = new Time();
         int a = 12;
         myTime.setHour(a);
         System.out.println(myTime.stringToScreen());
-
+        
+        //Test recursion
         Recursion myFactorial = new Recursion(3);
         System.out.println(myFactorial);
-        
+
+        //Test List
         List myList = new List();
+
+        //Test inner class
+        InnerClass test = new InnerClass();// class
+        InnerClass.doSomething test2 = test.new doSomething();
+        System.out.println(test2.i + " " + test2.s);
+        InnerClass.Flower test3 = test.new Flower("hi", 47);
+        System.out.println(test3.s + " " + test3.petalCount);
       
         
     }
