7bb8475316a907d4af64bee40c85b85b70360745
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-03-07 07:32:01 +0000

    arrange code

--- src/main/java/com/mycompany/app/Callback/CalleeTest.java
+++ src/main/java/com/mycompany/app/Callback/CalleeTest.java
@@ -1,10 +1,10 @@
 package com.mycompany.app.Callback;
 
 public class CalleeTest implements Increment{
-    private int i=0;
+    private int i=1;
     public void inMeth(){
         i++;
-        System.out.println("Callee with i"+i);
+        System.out.println("Callee with i = "+i);
 
     }
 
