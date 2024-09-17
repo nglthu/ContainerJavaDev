afef68c46fc68da7749fab981885a843a7d86569
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-09-17 02:30:31 +0000

    Java AWT

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -57,8 +57,27 @@ import com.mycompany.app.testConstructor;
 //import com.mycompany.app.Node.;
 //import com.mycompany.app.Node.;
 
+import java.awt.*;
+import java.awt.event.*;
+
 public class App {
+  public App() {
+    Button btn = new Button("Button");
+    btn.setBounds(50, 50, 50, 50);
+    add(btn);
+    setSize(150, 150);
+    setTitle("This is my First AWT example");
+    setLayout(new FlowLayout());
+    setVisible(true);
+    addWindowListener(new WindowAdapter() {
+        public void windowClosing(WindowEvent we) {
+            dispose();
+        }
+    });
+  }
+
   public static void main(String[] args) {
+    new App();
   /*  System.out.println("test");
     testConstructor testCon = new testConstructor();
     System.out.println(testCon.x);
